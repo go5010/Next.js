41cff2393692fc14a21599f2d8531ac736c86d32
go5010 <gota.4305@gmail.com>
2024-02-09 06:16:58 +0900

    20240209

--- next.js13-tailwindcss-udemy-blog/src/Data/blogAPI.ts
+++ next.js13-tailwindcss-udemy-blog/src/Data/blogAPI.ts
@@ -3,3 +3,5 @@ import { Article } from "./types";
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http:localhost:3001/posts", { cache: "no-store" }); //SSR
 };
+
+// 2
