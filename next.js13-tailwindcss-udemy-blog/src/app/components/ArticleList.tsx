22fc2c6f019871a46a537cb52dc5e62d3119ef02
go5010 <gota.4305@gmail.com>
2024-04-14 11:23:50 +0900

    part30完了

--- next.js13-tailwindcss-udemy-blog/src/app/components/ArticleList.tsx
+++ next.js13-tailwindcss-udemy-blog/src/app/components/ArticleList.tsx
@@ -10,11 +10,11 @@ type ArticleListProps = {
 const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      {articles.map((articles) => (
-        <article className="shadow my-4 flex flex-col" key={articles.id}>
-          <Link href={`articles/${articles.id}`} className="hover:opacity-75">
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=""
               width={1280}
               height={300}
@@ -25,22 +25,22 @@ const ArticleList = ({ articles }: ArticleListProps) => {
               Technology
             </Link>
             <Link
-              href={`articles/${articles.id}`}
+              href={`articles/${article.id}`}
               className="text-slate-900 text-3xl font-bold hover:text-gray-700"
             >
-              {articles.title}
+              {article.title}
             </Link>
             <p className="text-sm pb-3 text-slate-900">
-              Published on {articles.createdAt}
+              Published on {article.createdAt}
             </p>
             <Link
-              href={`articles/${articles.id}`}
+              href={`articles/${article.id}`}
               className="text-slate-900 pb-6"
             >
-              {articles.content}
+              {article.content}
             </Link>
             <Link
-              href={`articles/${articles.id}`}
+              href={`articles/${article.id}`}
               className="text-pink-800 hover:text-black"
             >
               続きを読む
